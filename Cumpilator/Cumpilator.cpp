49610e864147e7bd0270b1026b9f31d650786055
Mathieu KERBEL <40839921+Mideky-hub@users.noreply.github.com>
2023-11-30 10:02:23 +0100

    Ajoutez des fichiers projet.

---
+++ Cumpilator/Cumpilator.cpp
@@ -0,0 +1,12 @@
+﻿// Cumpilator.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Cumpilator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
