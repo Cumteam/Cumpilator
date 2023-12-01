a5e1d98226af15e48f88e27e1305003d317d7604
Paul Hariel <paul.hariel@outlook.com>
2023-12-01 17:06:12 +0100

    Create build command

--- cumpilator_cli/include/commands/build.h
+++ cumpilator_cli/include/commands/build.h
@@ -14,7 +14,20 @@ namespace cli
 	{
 		class build : public command
 		{
+		public:
+			build();
 
+			std::pair<bool, std::string> parse_arguments(const std::vector<std::string>& arguments) override;
+
+			std::string get_name() const override;
+			std::string alias() const override;
+			std::string get_help_message() const override;
+
+			std::pair<bool, std::string> execute() const override;
+
+		private:
+			std::vector<std::string> m_options;
+			std::vector<std::string> m_files;
 		};
 	}
 }
