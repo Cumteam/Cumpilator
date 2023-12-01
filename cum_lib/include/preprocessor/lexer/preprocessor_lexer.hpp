873f91469dea576c7b6e550383ccde73259d86b7
Mathieu KERBEL <40839921+Mideky-hub@users.noreply.github.com>
2023-12-01 09:34:44 +0100

    Resolve conflicts

--- cum_lib/include/preprocessor/lexer/preprocessor_lexer.hpp
+++ cum_lib/include/preprocessor/lexer/preprocessor_lexer.hpp
@@ -1,1 +1,22 @@
+/**
+ * @file preprocessor_lexer.hpp
+ * @author Mideky
+ * @brief 
+ *	Preprocessor lexer is a lexer that is used to lex the preprocessor directives in C++ source code.
+ * 
+ * @version 0.1
+ * @date 2023-12-01
+ *
+ * @copyright Copyright (c) 2023
+ */
 #pragma once
+
+class preprocessor_lexer
+{
+public:
+	preprocessor_lexer() = delete;
+	~preprocessor_lexer() = delete;
+private:
+	
+};
+
